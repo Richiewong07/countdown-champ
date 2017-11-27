058d8b5acf729467cb2731a667467819edda8873
Richie Wong <richiewong07@yahoo.com>
2017-11-26 22:09:37 -0600

    finished

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; // puts react code to browser
+import App from './App';
+
+ReactDOM.render(
+  <App/>, document.getElementById('root')
+);
